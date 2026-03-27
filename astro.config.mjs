22f82fe1a29c0d954313576208ed0421dd8c1300
Cesar do Nascimento <git@cesardonascimento.com>
2026-03-27 10:33:18 -0300

    melhorias gerais na interface, implementação de header e footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), db()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  redirects: {
+    "/": "/personagens",
+  },
 });
