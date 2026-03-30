cb576c7dd87eb435433ed212c152a06345eab405
Thailani Gomes <87913556+ThailaniGomes@users.noreply.github.com>
2026-03-30 12:22:50 -0300

    Feat: Criar componente de pesquisa (#9)
    * Migrar componente lista para react
    
    * Criar componente de pesquisa

---
+++ src/components/CharacterList.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import ListItem from "./ListItem";
+import { Field } from "./ui/field";
+import { Input } from "./ui/input";
+
+const CharacterList = () => {
+  const [characters, setCharacters] = useState([]);
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const chamada = await fetch("/api/characters");
+      const data = await chamada.json();
+      setCharacters(data.characters);
+      setFilteredCharacters(data.characters);
+    };
+    fetchData();
+  }, [setCharacters]);
+
+  const filterCharacters = (event: any) => {
+    const value = event.target.value;
+
+    setSearch(value);
+
+    if (value === "") {
+      setFilteredCharacters(characters);
+      return;
+    }
+
+    const filtered = characters.filter((character: any) => {
+      return character.name === value;
+    });
+
+    setFilteredCharacters(filtered);
+  };
+
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <div className="w-full">
+        <Field>
+          <Input
+            className="bg-gray-50"
+            placeholder="Pesquisa"
+            onChange={filterCharacters}
+          />
+        </Field>
+      </div>
+      {filteredCharacters.length === 0 && (
+        <div className="w-full text-center">
+          <p>{`Nenhum resultado encontrado para o termo: ${search}`}</p>
+        </div>
+      )}
+      {filteredCharacters.length > 0 && (
+        <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-8">
+          {filteredCharacters.map((character: any) => {
+            return (
+              <ListItem
+                title={character.name}
+                description={character.description}
+                imageUrl={character.imageUrl}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CharacterList;
